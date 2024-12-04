669a08ea5dc2e84558772b3b0141efcb7eb99481
Ian Higgins <ianrhiggins@gmail.com>
2024-12-03 16:32:05 -0800

    voltage date

--- app.py
+++ app.py
@@ -152,7 +152,7 @@ def telemetry_status(metadata):
         if response.status_code == 200:
             data = response.json()
             df = pd.DataFrame(data)
-            print(df.sort_values("voltage_date"))
+           
             #df["voltage_date"] = pd.to_datetime(df['voltage_date'])
             #df["voltage_date"]  = df["voltage_date"].dt.strftime('%Y-%m-%d %H:%M')
            
